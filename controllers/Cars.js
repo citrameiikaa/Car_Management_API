94cf460ca9b8948389437408a5951a9fe185be2f
citraaaca <citrameidaantika17@gmail.com>
2022-10-21 23:11:42 +0700

    half finish

--- controllers/Cars.js
+++ controllers/Cars.js
@@ -6,29 +6,46 @@ const Cars = db.Cars
 
 
 export const getCars = async (req, res) => {
-    if (req.user.role == "member") {
-        res.status(401).json({
-            status: "Unauthorized",
-            message: "You are not authorized to register an SuperAdmin / Admin",
-        });
-        return;
-    }
-    const cars = await Cars.findAll({
-        // attributes: ['id', 'name', 'email', 'role']
-        // {
-        //     "id": 7,
-        //     "name": "BMW",
-        //     "price": "5000",
-        //     "size": "large",
-        //     "available": 2,
-        //     "createdBy": null,
-        //     "updatedBy": null,
-        //     "deletedBy": null,
-        //     "createdAt": "2022-10-21T11:00:07.000Z",
-        //     "updatedAt": "2022-10-21T11:00:07.000Z"
-        // }
-    });
-    res.json(cars);
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == "") return res.sendStatus(401);
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) return res.sendStatus(403);
+        req.user = {
+            userId: decoded.userId,
+            name: decoded.name,
+            email: decoded.email,
+            role: decoded.role
+        }
+        req.email = decoded.email;
+        req.role = decoded.role;
+        req.name = decoded.name;
+        // next();
+    })
+    res.json(req.user);
+    // if (req.user.role == "member") {
+    //     res.status(401).json({
+    //         status: "Unauthorized",
+    //         message: "You are not authorized to register an SuperAdmin / Admin",
+    //     });
+    //     return;
+    // }
+    // const cars = await Cars.findAll({
+    // attributes: ['id', 'name', 'email', 'role']
+    // {
+    //     "id": 7,
+    //     "name": "BMW",
+    //     "price": "5000",
+    //     "size": "large",
+    //     "available": 2,
+    //     "createdBy": null,
+    //     "updatedBy": null,
+    //     "deletedBy": null,
+    //     "createdAt": "2022-10-21T11:00:07.000Z",
+    //     "updatedAt": "2022-10-21T11:00:07.000Z"
+    // }
+    // });
+    //     res.json(cars);
 }
 
 export const getCarsById = async (req, res) => {
@@ -51,14 +68,33 @@ export const getCarsById = async (req, res) => {
 }
 
 export const createCars = async (req, res) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == "") return res.sendStatus(401);
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) return res.sendStatus(403);
+        // req.user = {
+        //     userId: decoded.userId,
+        //     name: decoded.name,
+        //     email: decoded.email,
+        //     role: decoded.role
+        // }
+        req.userId = decoded.userId;
+        req.email = decoded.email;
+        req.role = decoded.role;
+        req.name = decoded.name;
+        // next();
+    })
     const {
         name,
         price,
         size,
         available
     } = req.body;
+    const createdBy = req.email;
     try {
         await Cars.create({
+            createdBy,
             name: name,
             price: price,
             size: size,
@@ -81,8 +117,10 @@ export const updateCars = async (req, res) => {
         name,
         price
     } = req.body;
+    const updatedBy = req.email;
     try {
         await Cars.update({
+            updatedBy,
             name: name,
             price: price
         }, {
@@ -100,10 +138,13 @@ export const updateCars = async (req, res) => {
 }
 
 export const deleteCars = async (req, res) => {
+    
     const {
         id
     } = req.params;
+    const deletedBy = req.email;
     const dataBeforeDelete = await Cars.findOne({
+        deletedBy,
         where: {
             id: id
         },
